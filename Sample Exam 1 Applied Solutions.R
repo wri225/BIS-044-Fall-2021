4fb36d21bf57b141cf4ab9ba9cc6f0cf33c3fd0c
Troy Adair <tra418@lehigh.edu>
2021-10-09 11:43:04 -0400

    Corrected Exam 1 Solutions

--- Sample Exam 1 Applied Solutions.R
+++ Sample Exam 1 Applied Solutions.R
@@ -1,11 +1,19 @@
+library(tidyverse)
 df <- read.csv("WMT.csv")
+END <-length(df$Low)
+# Need to fix problem with inconsistent dates
+for (i in 1:END) {
+  if (substr(df$Date[i],1,2)=="20"||substr(df$Date[i],1,2)=="19") {
+    df$Date[i] <- paste(substr(df$Date[i],6,7),"/", substr(df$Date[i],9,10),"/", substr(df$Date[i],1,4),sep='')
+  } else{}
+}
 
 # Q1
-END <-length(df$Low)
 END
 
 # Q2
-as.Date(df$Date[END]) - as.Date(df$Date[1])
+
+as.Date(df$Date[END], "%m/%d/%Y") - as.Date(df$Date[1],"%m/%d/%Y")
 
 # Q3
 price1 <- 0
